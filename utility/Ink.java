fe948028cb1a8c3133ed7d80d3aae5b1e0930113
Avinash sreedharan <Avinash.Sreedharan762@triosstudent.com>
2025-08-08 15:51:51 -0400

    updated weapon package

---
+++ utility/Ink.java
@@ -0,0 +1,47 @@
+package utility;
+
+
+public class Ink {
+
+    public Ink() {} // zero arg constructor  
+    public void welcome() {
+        System.out.println("////////////////////////////////////////////");
+        System.out.println("Welcome to the Battle of the Avengers!");
+        System.out.println("////////////////////////////////////////////\n");
+
+    } // welcome method
+    
+    public void goodbye() {
+        System.out.println("////////////////////////////////////////////");
+        System.out.println("////////// Thank you for playing! //////////");
+        System.out.println("////////////////////////////////////////////\n");
+        
+    } // goodbye method
+
+    public void warriorMenu() {
+        System.out.println("////////////////////////////////////////////");
+        System.out.println("//// Please choose your warrior type: //");
+        System.out.println("//// 1) Black Panther");
+        System.out.println("//// 2) Thor");
+        System.out.println("//// 3) Ironman");
+        System.out.println("////////////////////////////////////////////\n");
+    } // warriorMenu method   
+
+    public void attackMenu() {
+        System.out.println("////////////////////////////////////////////");
+        System.out.println("//// Select your attack type: /////");
+        System.out.println("//// 1) Normal Attack");
+        System.out.println("//// 2) Heavy Attack");
+        System.out.println("//// 3) Special Attack");
+        System.out.println("////////////////////////////////////////////\n");
+    } // attackMenu method   
+    public void weaponMenu() {
+        System.out.println("////////////////////////////////////////////");
+        System.out.println("//// Please choose your weapon: //");
+        System.out.println("//// 1) Claws");
+        System.out.println("//// 2) hammer");
+        System.out.println("//// 3) Repulsor");
+        System.out.println("////////////////////////////////////////////\n");
+    } // weaponMenu method
+
+} // Ink class
